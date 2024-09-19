5bee4afacf99ab12c3923fee90cbe419d816bb2b
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-09-19 21:04:04 +0530

    Email Library

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -14,36 +14,26 @@ class TestController extends CI_Controller
 
 	public function pdf()
 	{
-		$this->load->library('pdfmaker/makepdf');
+		$this->load->library('pdflib/makepdf');
 		$this->makepdf->init('P', 'mm', 'A4')->load('layout-2')->generate();
 	}
 
 	public function send()
 	{
-		$config['smtp_host'] = $this->config->item('brevo_smtp');
-		$config['smtp_user'] = $this->config->item('brevo_login');
-		$config['smtp_pass'] = $this->config->item('brevo_password');
-		$config['newline'] = "\r\n";
-		$config['protocol'] = 'smtp';
-		$config['mailtype'] = 'html';
-		$config['smtp_crypto'] = 'tls';
-		$config['smtp_port'] = $this->config->item('brevo_port');
-		$config['charset'] = 'iso-8859-1';
+		$this->load->library('email/brevomail');
+		$this->brevomail->init();
 
-		$this->load->library('email');
-		$this->email->initialize($config);
+		$this->brevomail->from('business@sociomark.in', 'Sociomark');
+		$this->brevomail->to('hemant@sociomark.in');
 
-		$this->email->from('business@sociomark.in', 'Sociomark');
-		$this->email->to('hemant@sociomark.in');
+		$this->brevomail->subject('Email Test');
+		$this->brevomail->message('Testing the brevomail class.');
 
-		$this->email->subject('Email Test');
-		$this->email->message('Testing the email class.');
-
-		if (!$this->email->send()) {
-			echo $this->email->print_debugger();
+		if (!$this->brevomail->send()) {
+			echo $this->brevomail->print_debugger();
 		} else {
 			echo "Success!";
 		}
-		$this->email->clear(TRUE);
+		$this->brevomail->clear(TRUE);
 	}
 }
