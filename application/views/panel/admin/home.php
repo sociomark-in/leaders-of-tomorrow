b7efbc1b5c7cfb92e82e75c5a55ea3c5e9b58f75
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-12-11 19:45:00 +0530

    Counter Tabs

--- application/views/panel/admin/home.php
+++ application/views/panel/admin/home.php
@@ -22,15 +22,14 @@
 							<div class="card">
 								<div class="card-body">
 									<div class="d-flex justify-content-between align-items-baseline">
-										<h6 class="card-title mb-0">Approved</h6>
+										<h6 class="card-title mb-0">All Nominations</h6>
 										<div>
 											<a class="btn p-0 btn-icon-text text-dark" href="<?= base_url('dashboard/applications') ?>">View All</a>
 										</div>
 									</div>
 									<div class="row">
 										<div class="col-12">
-											<h3 class="mb-2"><?= $applications_count['approved'] ?></h3>
-
+											<h3 class="mb-2"><?= $applications_count['all'] ?></h3>
 										</div>
 									</div>
 								</div>
@@ -44,7 +43,7 @@
 							<div class="card">
 								<div class="card-body">
 									<div class="d-flex justify-content-between align-items-baseline">
-										<h6 class="card-title mb-0">Uncategorized</h6>
+										<h6 class="card-title mb-0">New Nominations</h6>
 										<div>
 											<a class="btn p-0 btn-icon-text text-dark" href="<?= base_url('dashboard/applications') ?>">View All</a>
 										</div>
